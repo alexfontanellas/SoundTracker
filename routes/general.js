7a44c9f155fc41adf94bfc9d241302e65a0b7f36
Mi6u3l <miguelbragagomes@gmail.com>
2017-07-14 16:36:39 +0200

    add search results

--- routes/general.js
+++ routes/general.js
@@ -9,6 +9,11 @@ router.get("/", ensureLogin.ensureLoggedIn(), (req,res,next) => {
 });
 
 //Render search results
+router.get("/searchresults", ensureLogin.ensureLoggedIn(), (req,res,next) => {
+  res.render("searchresults");
+});
+
+//Render queue
 router.get("/resultsqueue", ensureLogin.ensureLoggedIn(),(req,res,next) => {
     var musicList = ['https://p.scdn.co/mp3-preview/6ab7825c891d399458e4068940eff436b67ae57b?cid=7c824c4a01474c8fbb1a67de125fffbc',
   'https://p.scdn.co/mp3-preview/74a0da99e8c1452cf915720f783cacefab7a66df?cid=7c824c4a01474c8fbb1a67de125fffbc',
