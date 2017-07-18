89237409141b8898f5e0a77afe05c78b779eafdd
Mi6u3l <miguelbragagomes@gmail.com>
2017-07-18 21:30:09 +0200

    add styles landing page

--- routes/spotifyCalls.js
+++ routes/spotifyCalls.js
@@ -1,7 +1,6 @@
 /* jshint esversion: 6 */
 const express = require('express');
 const router = express.Router();
-
 const User = require("../models/user");
 const Song = require("../models/song");
 
