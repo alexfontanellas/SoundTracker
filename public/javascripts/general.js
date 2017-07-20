c08208325dbffaddd19908773df290d741138c89
Mi6u3l <miguelbragagomes@gmail.com>
2017-07-20 11:43:53 +0200

    style fixing

--- public/javascripts/general.js
+++ public/javascripts/general.js
@@ -11,3 +11,13 @@ let initializeMaps = {};
 let marker = {};
 let map = {};
 let setGeoCoder = {};
+
+$(document).ready(function() {
+  $('div').removeClass('menu-active');
+  var pathname = window.location.pathname;  
+  if (pathname.length > 1) {
+    $('a[href="'+pathname+'"]').parent().addClass('menu-active');
+  } else {
+    $('a[href="/history"]').parent().addClass('menu-active');
+  }
+});
